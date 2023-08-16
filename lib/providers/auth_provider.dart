699bacfba8bfc3b7ea0010a6377d3a439a59f46b
book <book@192.168.1.3>
2023-08-16 22:39:04 +0545

    changes

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,6 +2,9 @@ import 'package:chatapp/models/common_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../service/auth_service.dart';
 
+final authProvider = StateNotifierProvider<AuthProvider, CommonState>(
+    (ref) => AuthProvider(CommonState.empty(), ref.watch(authService)));
+
 class AuthProvider extends StateNotifier<CommonState> {
   final AuthService service;
   AuthProvider(super.state, this.service);
