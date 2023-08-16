06fd57e7bb7e82f4cb13965facfd22f0a1adf8c2
book <book@192.168.1.4>
2023-08-16 20:32:18 +0545

    changes

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,19 +2,19 @@ import 'package:chatapp/models/common_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../service/auth_service.dart';
 
-
-
-
-
-class AuthProvider extends StateNotifier <CommonState> {
+class AuthProvider extends StateNotifier<CommonState> {
   final AuthService service;
   AuthProvider(super.state, this.service);
 
   Future<void> userLogin(
-      {required String email,
-        required String password
-      }) async {
+      {required String email, required String password}) async {
+    state = state.copyWith(
+        isLoad: true, errtext: '', isError: false, isSuccess: false);
     final response = await service.userLogin(email: email, password: password);
+    response.fold((l) {
+      (isLoad: false, errtext: l, isError: true, isSuccess: false);
+    }, (r) {
+      (isLoad: false, errtext: '', isError: false, isSuccess: r);
+    });
   }
-
 }
